84b3fb80aa1c57b50d9e7a5593bbbf38da4ca39c
c811k <bhk811@gmail.com>
2018-11-27 17:05:16 -0800

    added else if condition in javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,22 @@
+//Create a counter to track the user's score total
+var counter = 0;
+var targetNumber = 50;
+
+$("#number-to-guess").text(targetNumber);
+
 $(".crystal-image").on("click", function() {
 
-    alert("You clicked a crystal!");
+    counter+= 10;
+
+    alert("Your new score is " + counter);
+
+    if(counter === targetNumber) {
+        
+        alert("You win!");
+    }
+
+    else if(counter >= targetNumber) {
+        
+        alert("You lose!");
+    }
 });
